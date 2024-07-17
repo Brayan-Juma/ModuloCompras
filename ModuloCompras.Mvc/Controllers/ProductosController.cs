302a8f51b7684d8c5c8bf35c138b86ff90069acb
BRYAN JESUS JUMA QUILCA <bjjumaq@utn.edu.ec>
2024-07-16 21:28:19 -0500

    bryan

---
+++ ModuloCompras.Mvc/Controllers/ProductosController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using ModuloCompras.Mvc.Models;
+
+namespace ModuloCompras.Mvc.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiUrlProductos;
+
+        public ProductosController(IConfiguration configuration)
+        {
+            _httpClient = new HttpClient();
+            _apiUrlProductos = configuration["ApiUrlProductos"];
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync($"{_apiUrlProductos}/Productos");
+            response.EnsureSuccessStatusCode();
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var productos = JsonConvert.DeserializeObject<List<Producto>>(jsonResponse);
+            return View(productos);
+        }
+    }
+}
---
+++ ModuloCompras.Mvc/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿namespace ModuloCompras.Mvc.Models
+{
+    public class Producto
+    {
+        public int Id_Producto { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double GravavaIVA { get; set; }
+        public double Costo { get; set; }
+        public double Pvp { get; set; }
+        public bool Estado { get; set; }
+        public int StockProducto { get; set; }
+    }
+}

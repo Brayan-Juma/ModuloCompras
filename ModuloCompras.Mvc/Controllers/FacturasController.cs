b2f6a11b3bd4dfc1fb4e86028e620bc0e290b58a
BRYAN JESUS JUMA QUILCA <bjjumaq@utn.edu.ec>
2024-07-18 00:37:58 -0500

    bry

--- ModuloCompras.Mvc/Controllers/FacturasController.cs
+++ ModuloCompras.Mvc/Controllers/FacturasController.cs
@@ -1,18 +1,23 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ModuloCompras.ConsumeApi;
 using ModuloCompras.Entidades;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace ModuloCompras.Mvc.Controllers
 {
     public class FacturasController : Controller
     {
         private string urlApi;
+        private readonly HttpClient _httpClient;
 
         public FacturasController(IConfiguration configuration)
         {
             urlApi = configuration.GetValue("ApiUrlBase", "").ToString() + "/Facturas";
         }
+        
 
         // GET: FacturasController
         public ActionResult Index()
@@ -29,10 +34,10 @@ namespace ModuloCompras.Mvc.Controllers
         }
 
         // GET: FacturasController/Create
-        public ActionResult Create()
+       public ActionResult Create()
         {
             return View();
-        }
+       }
 
 
         // POST: FacturasController/Create
@@ -99,6 +104,10 @@ namespace ModuloCompras.Mvc.Controllers
                 return View(data);
             }
         }
+
+
+
+
     }
 }
 
